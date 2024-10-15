58ed4234ddab19a5bb72443587c00a1e39c679d5
aqsaqa <aqsaqayyum8627@gmail.com>
2024-10-14 20:41:25 -0700

    push my assg on github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,64 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Responsive(){
+  return(
+      <div className="parentContainer">
+          <div className="childContainer">
+              <div className="imageContainer" >
+            
+              <h1 className="title" style={{color:'white'}}>Antioxident Protection</h1>
+              
+              <p className="description">Vitamin c serum free radicles ko neutlize krk skin damge control krta hy.</p> 
+               <button className="readMore"> Read More</button>
+               </div>
+  
+          </div>
+          <div className="childContainer">
+              <div className="imageContainer">
+              <h1 className="title" style={{color:'white'}}>Skin Brihtening</h1>
+              <p className="description">vitamin c serum skin tone ko brighten krta hyor pigmentation ko reduce karta hy. </p> 
+              <button className="readMore"> Read More</button>
+               </div>
+  
+          </div>
+          <div className="childContainer">
+              <div className="imageContainer">
+              <h1 className="title" style={{color:'white'}}>Collagen Prodction</h1>
+              <p className="description">vitamin c serum collagen ko synthesis ko stimulate karta hy .</p> 
+              <button className="readMore"> Read More</button>
+               </div>
+  
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+          <div className="childContainer">
+              <div className="imageContainer">
+              <h1 className="title" style={{color:'white'}}>Hydration Source</h1>
+              <p className="description">Vitamin c serum skin ki moisture retention ko increase krta hy jis sy skin hydrate ho.</p> 
+              <button className="readMore"> Read More</button>
+               </div>
+  
+          </div>
+
+
+          <div className="childContainer">
+              <div className="imageContainer">
+              <h1 className="title" style={{color:'white'}}>Dark Spots</h1>
+              <p className="description">vitamin c serum dark spots,age spots,aur acne markes ko light karta hy.</p> 
+              <button className="readMore"> Read More</button>
+               </div>
+  
+          </div>
+
+
+          <div className="childContainer">
+              <div className="imageContainer">
+              <h1 className="title" style={{color:'white'}}>Anti Inflammantory </h1>
+              <p className="description">Vitamin c serum skin ki redness aur inflammation ko reduce karta hy .</p> 
+              <button className="readMore"> Read More</button>
+               </div>
+  
+          </div>
+  
+      </div>
+  )
+  }
